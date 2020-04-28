2821208d054042c9ce5dd63b02edec0d589a594b
Ronserro <33457400+Ronserro@users.noreply.github.com>
2020-04-28 10:40:32 +0200

    Initial commit

---
+++ src/iMat/iMatController.java
@@ -0,0 +1,19 @@
+
+package iMat;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuBar;
+
+public class iMatController implements Initializable {
+    
+    @FXML private MenuBar menuBar;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }
+    
+}
